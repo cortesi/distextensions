9fab4578daf4e63f3ef09f56c89660db7fe1a115
Aldo Cortesi <aldo@nullcube.com>
2007-06-20 12:33:27 +1000

    More code checkin.

--- distextensions/utils.py
+++ distextensions/utils.py
@@ -1,8 +1,60 @@
-import os.path
-
-def findPackages(path):
-    pass
 
 
-def findPackageData(path):
-    pass
+import fnmatch, os.path
+
+def _fnmatch(name, patternList):
+    for i in patternList:
+        if fnmatch.fnmatch(name, i):
+            return True
+    return False
+
+
+def _splitAll(path):
+    parts = []
+    h = path
+    while 1:
+        if not h:
+            break
+        h, t = os.path.split(h)
+        parts.append(t)
+    parts.reverse()
+    return parts
+
+
+def findPackages(path, dataExcludes=[]):
+    """
+        Recursively find all packages and data files rooted at path. Note that
+        only data _directories_ and their contents are returned - non-Python
+        files at module scope are not.
+
+        Excludes is a list of fnmatch-compatible expressions for files that
+        should not be included in the data directories return.
+    """
+    packages = []
+    datadirs = []
+    for root, dirs, files in os.walk(path, topdown=True):
+        if "__init__.py" in files:
+            p = _splitAll(root)
+            packages.append(".".join(p))
+        else:
+            dirs[:] = []
+            if packages:
+                datadirs.append(root)
+
+    # Now we recurse into the data directories
+    package_data = {}
+    for i in datadirs:
+        if not _fnmatch(i, dataExcludes):
+            parts = _splitAll(i)
+            module = ".".join(parts[:-1])
+            acc = []
+            for root, dirs, files in os.walk(i, topdown=True):
+                sub = _splitAll(root)[1:]
+                for fname in files:
+                    path = sub + [fname]
+                    path = os.path.join(*path)
+                    acc.append(path)
+            package_data[module] = acc
+    return packages, package_data
+
+
--- test/test_utils.py
+++ test/test_utils.py
@@ -1,6 +1,37 @@
 import pylid
-import distextensions
+from distextensions.utils import *
+from distextensions.utils import _fnmatch, _splitAll
+
 
 class testFindPackages(pylid.TestCase):
-    def test_foo(self):
-        pass
+    def test_testmodule(self):
+        p, d = findPackages("testmodule")
+        assert p == ["testmodule", "testmodule.subpackage"]
+        assert d == {
+            "testmodule": [
+                'pkgdata/foo', 'pkgdata/falsemod/__init__.py'
+            ]
+        }
+
+    def test_emptymod(self):
+        p, d = findPackages("testmodule/pkgdata")
+        assert not p
+
+
+class u_fnmatch(pylid.TestCase):
+    def test_match(self):
+        assert not _fnmatch("foo", [])
+        assert _fnmatch("foo", ["*oo"])
+        assert _fnmatch("foo", ["bar", "*oo"])
+        assert not _fnmatch("mong", ["bar", "*oo"])
+
+
+class u_splitAll(pylid.TestCase):
+    def _roundtrip(self, *lst):
+        j = os.path.join(*lst)
+        self.assertEqual(_splitAll(j), list(lst))
+        
+    def test_splitAll(self):
+        self._roundtrip("foo", "bar", "voing")
+        self._roundtrip("foo", "bar")
+        self._roundtrip("foo")
