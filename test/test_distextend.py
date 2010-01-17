1afa6e816e257b7712df4bfefb21e8aa2d5c683d
Aldo Cortesi <aldo@nullcube.com>
2010-01-18 12:05:26 +1300

    Move to Pry.

--- test/test_distextend.py
+++ test/test_distextend.py
@@ -1,30 +1,30 @@
-import pylid
+import libpry
 from distextend import *
 from distextend import _fnmatch, _splitAll
 
 
-class testFindPackages(pylid.TestCase):
+class uFindPackages(libpry.AutoTree):
     def test_testmodule(self):
         p, d = findPackages("testmodule", dataExclude=["*.exclude"])
         assert p == ["testmodule", "testmodule.subpackage"]
-        self.assertEqual(
-            d,
-            {
+        expected = {
             "testmodule": [
-                                'pkgdata/foo',
                                 'pkgdata/falsemod/__init__.py',
+                                'pkgdata/foo',
                                 'pkgdata2/one/one',
                                 'pkgdata2/two/two',
                             ]
             }
-        )
+        for i in d.keys():
+            d[i].sort()
+        assert d == expected
 
     def test_emptymod(self):
         p, d = findPackages("testmodule/pkgdata", dataExclude=["*.exclude"])
         assert not p
 
 
-class u_fnmatch(pylid.TestCase):
+class u_fnmatch(libpry.AutoTree):
     def test_match(self):
         assert not _fnmatch("foo", [])
         assert _fnmatch("foo", ["*oo"])
@@ -32,12 +32,22 @@ class u_fnmatch(pylid.TestCase):
         assert not _fnmatch("mong", ["bar", "*oo"])
 
 
-class u_splitAll(pylid.TestCase):
+class u_splitAll(libpry.AutoTree):
     def _roundtrip(self, *lst):
         j = os.path.join(*lst)
-        self.assertEqual(_splitAll(j), list(lst))
+        assert _splitAll(j) == list(lst)
         
     def test_splitAll(self):
         self._roundtrip("foo", "bar", "voing")
         self._roundtrip("foo", "bar")
         self._roundtrip("foo")
+
+
+tests = [
+    uFindPackages(),
+    u_splitAll(),
+    u_fnmatch(),
+
+
+
+]
